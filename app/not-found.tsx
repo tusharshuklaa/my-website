0f14f759b7cc0f4bbcc8353177eb1a3cd97f40c7
Tushar Shukla <tushar.shukla@adidas.com>
2024-09-13 23:47:14 +0530

    Added 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { BackgroundBeams } from "@/components/ui/background-beams";
+import { GlitchText } from "@/components/glitch-text";
+import { Underline } from "@/components/underline";
+
+export default async function NotFound() {
+  return (
+    <div className="relative flex h-screen w-full flex-col items-center justify-center rounded-md bg-neutral-950 antialiased">
+      <div className="mx-auto max-w-4xl p-4">
+        <GlitchText className="mb-8 bg-gradient-to-b from-neutral-200 to-neutral-600 bg-clip-text font-sans text-8xl font-bold text-transparent">
+          404: NOT FOUND!
+        </GlitchText>
+        <p></p>
+        <p className="relative z-10 mx-auto my-2 max-w-lg text-center text-neutral-500">
+          Oops! The page you&apos;re looking for is either moved or doesn&apos;t
+          exist. <br />
+          If you think this is a mistake, please{" "}
+          <Link href="/contact">
+            <Underline>contact me</Underline>
+          </Link>
+          . <br />
+          <br />
+          Feel free to checkout the{" "}
+          <Link href="/">
+            <Underline>rest of my site</Underline>
+          </Link>{" "}
+          :)
+        </p>
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
+}
---
+++ components/underline.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { cn } from "@/lib/utils";
+
+type UnderlineSize = "sm" | "md" | "lg";
+type UnderlineProps = {
+  children: string;
+  className?: string;
+  size?: UnderlineSize;
+};
+
+export const Underline: FC<UnderlineProps> = ({
+  className,
+  children,
+  size = "sm",
+}) => {
+  const sizeClassMap: Record<UnderlineSize, string> = {
+    md: "after:-bottom-[0.125rem] after:h-3 after:-left-2 after:-right-2",
+    sm: "after:-bottom-1 after:h-2 after:-left-1 after:-right-1",
+    lg: "after:-bottom-[0.125rem] after:h-3 after:-left-2 after:-right-2",
+  };
+
+  return (
+    <span
+      className={cn(
+        "atfer:bg-cover relative after:absolute after:-z-[1] after:-rotate-[2deg] after:bg-[url('/img/underline.svg')] after:bg-no-repeat after:content-['']",
+        sizeClassMap[size],
+        className,
+      )}
+    >
+      {children}
+    </span>
+  );
+};
