1b3941c70a5bfd7fb89784873578c0b03739c87b
Tushar Shukla <tushar.shukla@adidas.com>
2024-12-23 22:30:22 +0530

    fixed timeline for mobile

--- components/text/underline.tsx
+++ components/text/underline.tsx
@@ -6,7 +6,7 @@ export const Underline: FC<BasicUiComponent> = ({ className, children }) => {
   return (
     <span
       className={cn(
-        "duration-250 bg-gradient-to-r from-[#84fab0] to-[#8fd3f4] bg-[length:100%_0.2em] bg-[position:0_88%] bg-no-repeat transition-[background-size] ease-in hover:bg-[length:100%_88%] grow-0 dark:hover:text-black",
+        "duration-250 bg-gradient-to-r from-[#84fab0] to-[#8fd3f4] bg-[length:100%_0.2em] bg-[position:0_100%] md:bg-[position:0_88%] bg-no-repeat transition-[background-size] ease-in hover:bg-[length:100%_88%] grow-0 dark:hover:text-black",
         className,
       )}
     >
--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -41,18 +41,15 @@ export const Timeline: FC<TimelineProps> = ({ className, containerRef, data, tar
     <div ref={itemsRef} className={cn("relative mx-auto max-w-7xl pb-20", className)}>
       {data.map((item, index) => (
         <div key={index} className="flex justify-start pt-10 md:gap-10 md:pt-20">
-          <div className="sticky top-80 z-40 flex max-w-xs flex-col items-center self-start md:w-full md:flex-row lg:max-w-sm">
-            <div className="absolute left-3 flex h-10 w-10 items-center justify-center rounded-full bg-white dark:bg-lightYellow md:left-3">
-              <div className="h-4 w-4 rounded-full border border-neutral-300 bg-neutral-200 p-2 dark:border-neutral-700 dark:bg-neutral-800" />
+          <div className="sticky top-40 sm:top-60 md:top-80 z-40 flex max-w-xs flex-col items-center self-start md:w-full md:flex-row lg:max-w-sm">
+            <div className="absolute left-2 flex h-6 md:h-10 w-6 md:w-10 items-center justify-center rounded-full bg-white dark:bg-lightYellow md:left-3">
+              <div className="h-1 md:h-4 w-1 md:w-4 rounded-full border border-neutral-300 bg-neutral-200 p-1 md:p-2 dark:border-neutral-700 dark:bg-neutral-800" />
             </div>
 
             <GradientText text={item.title} className="hidden text-xl md:block md:pl-20 md:text-5xl" color="pink" />
           </div>
 
-          <div className="relative w-full pl-20 pr-4 md:pl-4">
-            <h3 className="mb-4 block text-left text-2xl font-bold text-neutral-500 [text-shadow:_-0.025em_0_0.125rem_white] dark:text-neutral-500 md:hidden">
-              {item.title}
-            </h3>
+          <div className="relative w-full pl-12 pr-4 sm:pl-16 md:pl-4">
             <GradientText text={item.title} className="mb-4 block text-left text-2xl font-bold md:hidden" />
 
             <motion.div
@@ -82,7 +79,7 @@ export const Timeline: FC<TimelineProps> = ({ className, containerRef, data, tar
         style={{
           height: height + "px",
         }}
-        className="absolute left-8 top-0 w-[2px] overflow-hidden bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] dark:via-neutral-700 md:left-8"
+        className="absolute left-5 md:left-8 top-0 w-[2px] overflow-hidden bg-[linear-gradient(to_bottom,var(--tw-gradient-stops))] from-transparent from-[0%] via-neutral-200 to-transparent to-[99%] [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)] dark:via-neutral-700"
       >
         <motion.div
           style={{
