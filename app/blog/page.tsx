22b0cdb7dad4a3fd95c1a76f402e215cdb0eec1f
Tushar Shukla <tushar.shukla@adidas.com>
2024-12-27 00:32:01 +0530

    Added blog landing page with UI

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,42 +1,112 @@
-import { FC } from 'react';
-import { compareDesc, format, parseISO } from 'date-fns'
-import { allBlogs, Blog } from '@content';
-import { Card, CardDescription, CardTitle } from '@ui';
-import Link from 'next/link';
-import { Navbar } from '@/components/navbar';
+"use client";
 
-const BlogCard: FC<Blog> = (blog: Blog) => {
-  return (
-    <Card>
-      <CardTitle>
-        <Link href={blog.url}>
-          {blog.title}
-        </Link>
-      </CardTitle>
-      <CardDescription>{blog.summary}</CardDescription>
-      <time dateTime={blog.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(blog.date), 'LLLL d, yyyy')}
-      </time>
-      <p>{blog.summary}</p>
-      <p>{blog.published}</p>
-    </Card>
-  );
-};
+import { FC } from 'react';
+import { compareDesc, format, parseISO } from 'date-fns';
+
+import { allBlogs } from '@content';
+import { BentoGrid, BentoGridItem, HoverCards, PlaceholdersAndVanishInput } from '@ui';
+import { cn } from '@/lib/utils';
+import { BlogHero } from '@components/blog-hero';
+import { BentoThreeDCard } from '@components/bento-three-d-card';
+import { LuminenceSkeleton, AnimatedContentSkeleton, ChatSkeleton, ImageSkeleton } from '@components/bento-skeleton';
+
+const placeholders = [
+  "react",
+  "performance",
+  "css art",
+  "productivity",
+];
 
 const AllBlogsPage: FC = () => {
-  const blogs = allBlogs.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  // Pick blogs that are published
+  const blogs = allBlogs
+    .filter(blog => blog.published)
+    .map(blog => ({
+      ...blog,
+      date: format(parseISO(blog.date), 'LLLL d, yyyy')
+    }))
+    .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  const recentBlogs = blogs.slice(0, 5).map((blog, index) => {
+    const itemClasses = cn(
+      'bg-white dark:bg-black',
+      {
+        'md:col-span-2 md:row-span-2': index === 0,
+        'md:col-span-2': index === 4,
+        'md:col-span-1': !(index === 0 || index === 4),
+      },
+    );
+
+    let Skeleton: () => JSX.Element = LuminenceSkeleton;
+    switch (index) {
+      case 0:
+        // Case for 3d card
+        break;
+      case 1:
+        Skeleton = AnimatedContentSkeleton;
+        break;
+      case 2:
+        Skeleton = ChatSkeleton;
+        break;
+      case 3:
+        Skeleton = LuminenceSkeleton;
+        break;
+      case 4:
+        Skeleton = () => ImageSkeleton(blog.title, blog.img);
+        break;
+      default:
+        Skeleton = LuminenceSkeleton;
+    };
+
+    return {
+      title: blog.title,
+      description: blog.summary,
+      header: <Skeleton />,
+      className: itemClasses,
+      date: blog.date,
+      img: blog.img,
+      url: blog.url,
+    };
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
 
   return (
-    <div className="">
-      <Navbar />
-      <div className="container mx-auto p-4">
-        {
-          blogs.map((blog, index) => (
-            <BlogCard key={index} {...blog} />
-          ))
-        }
-      </div>
-    </div>
+    <section className="relative mt-40 max-w-4xl mx-auto">
+      <BlogHero text="My Blogs" />
+
+      <PlaceholdersAndVanishInput
+        placeholders={placeholders}
+        onChange={handleChange}
+        onSubmit={onSubmit}
+        className="mb-32 mt-8"
+      />
+
+      <BentoGrid className="mb-4">
+        {recentBlogs.map((item, i) => {
+          if (i === 0) {
+            return <BentoThreeDCard key={i} {...item} />;
+          }
+
+          return (
+            <BentoGridItem
+              {...item}
+              key={i}
+              maxLines={2}
+              className={cn("[&>p:text-lg]", item.className)}
+            />
+          );
+        })}
+      </BentoGrid>
+
+      <HoverCards items={blogs.splice(5)} />
+    </section>
   );
 };
 
