63b4c332df5f2434972abb94181be171114686f8
Tushar Shukla <tushar.shukla@adidas.com>
2025-01-14 20:52:04 +0530

    Designed base uses page

--- app/(primaryLayout)/uses/page.tsx
+++ app/(primaryLayout)/uses/page.tsx
@@ -1,8 +1,17 @@
 import { FC } from "react";
+import { FileCode2, MonitorCog, PocketKnife } from 'lucide-react';
+import { MechaCard } from "@components/mecha-card";
+import { SnapSection } from "@components/snap-container";
 
 const UsesPage: FC = () => {
   return (
-    <div>Hello!!</div>
+    <SnapSection className="min-h-screen w-full pt-32 relative px-4 md:px-8 flex justify-center items-center">
+      <div className="flex justify-center items-center lg:gap-x-24 flex-wrap">
+        <MechaCard className="my-32" text="Coding" icon={<FileCode2 />} href="/uses/coding" />
+        <MechaCard className="my-32" text="Gadgets" icon={<PocketKnife />} href="/uses/gadgets" />
+        <MechaCard className="my-32" text="Software" icon={<MonitorCog />} href="/uses/software" />
+      </div>
+    </SnapSection>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
         "background-position-spin": "background-position-spin 3000ms infinite alternate",
         "gradient-shift": "gradient-shift 5s ease-in-out infinite",
         "blur-animation": "blur-animation 3s ease-in-out alternate infinite",
-        marquee: 'marquee 15s linear infinite',
+        marquee: "marquee 15s linear infinite",
         "slider-anim": "slider-anim 20s linear infinite",
         shimmer: "shimmer 2s linear infinite",
         spotlight: "spotlight 2s ease .75s 1 forwards",
@@ -340,19 +340,19 @@ const config: Config = {
         },
         "slider-anim": {
           "0%": {
-            transform: "perspective(1000px) rotateX(-14deg) rotateY(0deg)"
+            transform: "perspective(1000px) rotateX(-14deg) rotateY(0deg)",
           },
           "100%": {
-            transform: "perspective(1000px) rotateX(-14deg) rotateY(360deg)"
+            transform: "perspective(1000px) rotateX(-14deg) rotateY(360deg)",
           },
         },
         shimmer: {
           from: {
-            "backgroundPosition": "0 0"
+            backgroundPosition: "0 0",
           },
           to: {
-            "backgroundPosition": "-200% 0"
-          }
+            backgroundPosition: "-200% 0",
+          },
         },
         spotlight: {
           "0%": {
@@ -384,6 +384,7 @@ const config: Config = {
       transitionTimingFunction: {
         "in-text": "cubic-bezier(0.445, 0.05, 0.55, 0.95)",
         "in-letter": "cubic-bezier(0.5, 0, 0, 1)",
+        "door-open": "cubic-bezier(0.785, 0.135, 0.15, 0.86)",
       },
     },
   },
@@ -395,21 +396,21 @@ const config: Config = {
         {
           "bg-grid": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
             )}")`,
           }),
           "bg-grid-small": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
             )}")`,
           }),
           "bg-dot": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
             )}")`,
           }),
         },
-        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" },
       );
     },
     backfaceVisibility,
