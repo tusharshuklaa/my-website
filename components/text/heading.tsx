e3fd38c98ab44b9b403249ce47d5c2dc1de272f1
Tushar Shukla <tushar.shukla@adidas.com>
2025-02-05 23:33:16 +0530

    merge conflict fixes

--- components/text/heading.tsx
+++ components/text/heading.tsx
@@ -20,26 +20,6 @@ export const AnimatedHeading: FC<BasicUiComponent> = ({ children, className }) =
         ease: "easeInOut",
       }}
       className={cn(
-        "mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-3xl sm:text-4xl font-medium tracking-tight text-transparent md:text-7xl m-auto max-w-[90%] sm:max-w-none",
-        className
-      )}
-    >
-      {children}
-    </motion.h1>
-  );
-};
-
-export const AnimatedHeading: FC<BasicUiComponent> = ({ children, className }) => {
-  return (
-    <motion.h1
-      initial={{ opacity: 0.5, y: 100 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{
-        delay: 0.3,
-        duration: 0.8,
-        ease: "easeInOut",
-      }}
-      className={cn(
         "mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl",
         className
       )}
